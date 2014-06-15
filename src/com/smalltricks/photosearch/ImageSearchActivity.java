9f4c2f1d69dc742c8291f6d25386bf147e73d784
Chintan Sheth <chintansheth87@gmail.com>
2014-06-15 11:53:11 -0700

    first commit

---
+++ src/com/smalltricks/photosearch/ImageSearchActivity.java
@@ -0,0 +1,15 @@
+package com.smalltricks.photosearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ImageSearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_search_activity);
+    }
+}
