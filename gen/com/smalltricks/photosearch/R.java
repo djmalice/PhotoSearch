29e97fa4c1f269592be55e3d95993e350a4abff3
Chintan Sheth <chintansheth87@gmail.com>
2014-06-15 23:02:21 -0700

    Added display of images. Can open images in fullscreen

--- gen/com/smalltricks/photosearch/R.java
+++ gen/com/smalltricks/photosearch/R.java
@@ -14,15 +14,38 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060001;
-        public static final int editText1=0x7f060000;
+        public static final int Spinner01=0x7f070002;
+        public static final int btSaveSettings=0x7f07000a;
+        public static final int btSearch=0x7f07000c;
+        public static final int editText1=0x7f070009;
+        public static final int etSearchText=0x7f07000b;
+        public static final int gvResults=0x7f07000d;
+        public static final int ivResult=0x7f070000;
+        public static final int spinner1=0x7f070003;
+        public static final int spinner2=0x7f070007;
+        public static final int tvImageSizeLabel=0x7f070004;
+        public static final int tvImageTypeLabel=0x7f070006;
+        public static final int tvSearchLabel=0x7f070001;
+        public static final int tvSiteFilterLabel=0x7f070008;
+        public static final int tvcolorFilterLabel=0x7f070005;
     }
     public static final class layout {
-        public static final int image_search_activity=0x7f030000;
+        public static final int activity_full_image=0x7f030000;
+        public static final int activity_settings=0x7f030001;
+        public static final int image_search_activity=0x7f030002;
+        public static final int item_image_result=0x7f030003;
+    }
+    public static final class menu {
+        public static final int main_menu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int btSearch=0x7f040002;
+        public static final int etSearchHint=0x7f040003;
         public static final int hello_world=0x7f040001;
+        public static final int title_activity_full_image=0x7f040006;
+        public static final int title_activity_settings=0x7f040004;
+        public static final int tvSearchLabel=0x7f040005;
     }
     public static final class style {
         /** 
--- src/com/smalltricks/photosearch/ImageSearchActivity.java
+++ src/com/smalltricks/photosearch/ImageSearchActivity.java
@@ -1,15 +1,91 @@
 package com.smalltricks.photosearch;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class ImageSearchActivity extends Activity {
-
+	EditText etSearchText;
+	GridView gvResults;
+	Button btSearch;
+	ArrayList<ImageResult> imageResults = new ArrayList<ImageResult>();
+	ImageResultArrayAdapter imageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_search_activity);
+        setupViews();
+        imageAdapter = new ImageResultArrayAdapter(this, imageResults);
+        gvResults.setAdapter(imageAdapter);
+        gvResults.setOnItemClickListener(new OnItemClickListener() {
+        	@Override
+        	public void onItemClick(AdapterView<?> parent, View view,
+        			int position, long id) {
+        		// TODO Auto-generated method stub
+        	Intent i = new Intent(getApplicationContext(), FullImage.class);
+        	ImageResult imageResult = imageResults.get(position);
+        	i.putExtra("url",imageResult.getFullUrl());
+        	startActivity(i);
+        	}
+		});
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	// TODO Auto-generated method stub
+    	return super.onOptionsItemSelected(item);
+    }
+    public void setupViews(){
+    	etSearchText = (EditText)findViewById(R.id.etSearchText);
+    	gvResults = (GridView)findViewById(R.id.gvResults);
+    	btSearch = (Button)findViewById(R.id.btSearch);
+    }
+    
+    public void searchForResults(View v){
+    	String searchQuery = etSearchText.getText().toString();
+    	
+    	AsyncHttpClient client = new AsyncHttpClient();
+    	client.get("https://ajax.googleapis.com/ajax/services/search/images?rsz=8&" + 
+    			"start=" + 0 + "&v=1.0&q=" + Uri.encode(searchQuery), 
+    			new JsonHttpResponseHandler() {
+    			@Override
+    			public void onSuccess(JSONObject response){
+    				JSONArray imageJsonResults = null;
+    				try {
+						imageJsonResults = response.getJSONObject("responseData").getJSONArray("results");
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+    				imageResults.clear();
+    				imageAdapter.addAll(ImageResult.fromJSONArray(imageJsonResults));
+    				Log.d("DEBUG",imageResults.toString());
+    			}
+    		
+    			});
+    Log.d("DEBUG","https://ajax.googleapis.com/ajax/services/search/images?rsz=8&" + 
+			"start=" + 0 + "v=1.0&q=" + Uri.encode(searchQuery));	
+    	
+    	
+    	
+    	
+    	
     }
 }
