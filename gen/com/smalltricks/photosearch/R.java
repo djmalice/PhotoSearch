834d44b7c2fde9ea38c2ec477c117e40c36aa53f
Chintan Sheth <chintansheth87@gmail.com>
2014-06-18 10:20:46 -0700

    Completed base use-cases. Added search button in action bar

--- gen/com/smalltricks/photosearch/R.java
+++ gen/com/smalltricks/photosearch/R.java
@@ -8,26 +8,35 @@
 package com.smalltricks.photosearch;
 
 public final class R {
+    public static final class array {
+        public static final int saColorFilter=0x7f050001;
+        public static final int saImageSize=0x7f050000;
+        public static final int saImageType=0x7f050002;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_settings=0x7f020002;
     }
     public static final class id {
-        public static final int Spinner01=0x7f070002;
-        public static final int btSaveSettings=0x7f07000a;
-        public static final int btSearch=0x7f07000c;
-        public static final int editText1=0x7f070009;
-        public static final int etSearchText=0x7f07000b;
-        public static final int gvResults=0x7f07000d;
-        public static final int ivResult=0x7f070000;
-        public static final int spinner1=0x7f070003;
-        public static final int spinner2=0x7f070007;
-        public static final int tvImageSizeLabel=0x7f070004;
-        public static final int tvImageTypeLabel=0x7f070006;
-        public static final int tvSearchLabel=0x7f070001;
-        public static final int tvSiteFilterLabel=0x7f070008;
-        public static final int tvcolorFilterLabel=0x7f070005;
+        public static final int action_search=0x7f08000f;
+        public static final int btSaveSettings=0x7f08000a;
+        public static final int btSearch=0x7f08000c;
+        public static final int etSearchText=0x7f08000b;
+        public static final int etSiteFilter=0x7f080009;
+        public static final int gvResults=0x7f08000d;
+        public static final int item1=0x7f08000e;
+        public static final int ivResult=0x7f080000;
+        public static final int spColorFilter=0x7f080005;
+        public static final int spImageSize=0x7f080003;
+        public static final int spImageType=0x7f080007;
+        public static final int tvColorFilterLabel=0x7f080004;
+        public static final int tvImageSizeLabel=0x7f080002;
+        public static final int tvImageTypeLabel=0x7f080006;
+        public static final int tvSearchLabel=0x7f080001;
+        public static final int tvSiteFilterLabel=0x7f080008;
     }
     public static final class layout {
         public static final int activity_full_image=0x7f030000;
@@ -36,7 +45,7 @@ public final class R {
         public static final int item_image_result=0x7f030003;
     }
     public static final class menu {
-        public static final int main_menu=0x7f060000;
+        public static final int main_menu=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -69,10 +78,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/smalltricks/photosearch/ImageSearchActivity.java
+++ src/com/smalltricks/photosearch/ImageSearchActivity.java
@@ -7,17 +7,24 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -28,10 +35,12 @@ public class ImageSearchActivity extends Activity {
 	Button btSearch;
 	ArrayList<ImageResult> imageResults = new ArrayList<ImageResult>();
 	ImageResultArrayAdapter imageAdapter;
+	SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_search_activity);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
         setupViews();
         imageAdapter = new ImageResultArrayAdapter(this, imageResults);
         gvResults.setAdapter(imageAdapter);
@@ -46,7 +55,74 @@ public class ImageSearchActivity extends Activity {
         	startActivity(i);
         	}
 		});
+        
+        // Endless Scrolling
+        gvResults.setOnScrollListener(new EndlessScrollListener() {
+			
+			@Override
+			public void onLoadMore(int page, int totalItemsCount) {
+				// TODO Auto-generated method stub
+				customLoadMoreDataApi(totalItemsCount);
+			}
+		});
     }
+    
+    public void customLoadMoreDataApi(int offset){
+    	String searchQuery = etSearchText.getText().toString();
+    	
+    	String imageSize = pref.getString("imageSize", "");
+    	String imageType = pref.getString("imageType", "");
+    	String colorFilter = pref.getString("colorFilter", "");
+    	String siteFilter = pref.getString("siteFilter", "");
+    	if(isNetworkAvailable()){
+    		AsyncHttpClient client = new AsyncHttpClient();
+        	String urlParams = new String("&imgsz=" + imageSize + "&imgtype=" + imageType + "&imgcolor=" + colorFilter + "&as_sitesearch=" + siteFilter);
+        	
+        	client.get("https://ajax.googleapis.com/ajax/services/search/images?rsz=8&" + 
+        			"start=" + offset + "&v=1.0&q=" + Uri.encode(searchQuery) + urlParams, 
+        			new JsonHttpResponseHandler() {
+        			@Override
+        			public void onSuccess(JSONObject response){
+        				JSONArray imageJsonResults = null;
+        				try {
+    						imageJsonResults = response.getJSONObject("responseData").getJSONArray("results");
+    					} catch (JSONException e) {
+    						// TODO Auto-generated catch block
+    						e.printStackTrace();
+    					}
+        				if(imageJsonResults != null)
+        					imageAdapter.addAll(ImageResult.fromJSONArray(imageJsonResults));
+        				
+        				Log.d("DEBUG",imageResults.toString());
+        			}
+        		
+        			});
+        Log.d("DEBUG","https://ajax.googleapis.com/ajax/services/search/images?rsz=8&" + 
+    			"start=" + 0 + "&v=1.0&q=" + Uri.encode(searchQuery)+ urlParams);
+    	}
+     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	// TODO Auto-generated method stub
+    	getMenuInflater().inflate(R.menu.main_menu, menu);
+    	return super.onCreateOptionsMenu(menu);
+    }
+    
+    public void showSettings(MenuItem item){
+    	Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
+    	startActivity(i);
+    }
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	// TODO Auto-generated method stub
@@ -58,34 +134,58 @@ public class ImageSearchActivity extends Activity {
     	btSearch = (Button)findViewById(R.id.btSearch);
     }
     
+    private Boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager 
+              = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+    
     public void searchForResults(View v){
     	String searchQuery = etSearchText.getText().toString();
     	
-    	AsyncHttpClient client = new AsyncHttpClient();
-    	client.get("https://ajax.googleapis.com/ajax/services/search/images?rsz=8&" + 
-    			"start=" + 0 + "&v=1.0&q=" + Uri.encode(searchQuery), 
-    			new JsonHttpResponseHandler() {
-    			@Override
-    			public void onSuccess(JSONObject response){
-    				JSONArray imageJsonResults = null;
-    				try {
-						imageJsonResults = response.getJSONObject("responseData").getJSONArray("results");
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-    				imageResults.clear();
-    				imageAdapter.addAll(ImageResult.fromJSONArray(imageJsonResults));
-    				Log.d("DEBUG",imageResults.toString());
-    			}
-    		
-    			});
-    Log.d("DEBUG","https://ajax.googleapis.com/ajax/services/search/images?rsz=8&" + 
-			"start=" + 0 + "v=1.0&q=" + Uri.encode(searchQuery));	
-    	
-    	
-    	
-    	
-    	
+    	String imageSize = pref.getString("imageSize", "");
+    	String imageType = pref.getString("imageType", "");
+    	String colorFilter = pref.getString("colorFilter", "");
+    	String siteFilter = pref.getString("siteFilter", "");
+		if (isNetworkAvailable()) {
+			AsyncHttpClient client = new AsyncHttpClient();
+			String urlParams = new String("&imgsz=" + imageSize + "&imgtype="
+					+ imageType + "&imgcolor=" + colorFilter
+					+ "&as_sitesearch=" + siteFilter);
+
+			client.get(
+					"https://ajax.googleapis.com/ajax/services/search/images?rsz=8&"
+							+ "start=" + 0 + "&v=1.0&q="
+							+ Uri.encode(searchQuery) + urlParams,
+					new JsonHttpResponseHandler() {
+						@Override
+						public void onSuccess(JSONObject response) {
+							JSONArray imageJsonResults = null;
+							try {
+								imageJsonResults = response.getJSONObject(
+										"responseData").getJSONArray("results");
+							} catch (JSONException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							if (imageJsonResults != null) {
+								imageResults.clear();
+								imageAdapter.addAll(ImageResult
+										.fromJSONArray(imageJsonResults));
+							}
+							Log.d("DEBUG", imageResults.toString());
+						}
+
+					});
+
+			Log.d("DEBUG",
+					"https://ajax.googleapis.com/ajax/services/search/images?rsz=8&"
+							+ "start=" + 0 + "&v=1.0&q="
+							+ Uri.encode(searchQuery) + urlParams);
+    	} else {
+    		Toast.makeText(this,"Check Network Connectivity",Toast.LENGTH_SHORT).show();
+    	}
+ 	
     }
 }
